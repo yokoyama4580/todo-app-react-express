28c76c71803e0a36c6ed972cea54612bfb94ea3f
横山湧 <yoko0@MacBook-Air.local>
2025-04-09 22:21:14 +0900

    [add]add delete button(Frontend)
    フロント側の削除機能を実装しました

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -21,24 +21,69 @@ function App() {
       .catch((error) => console.error('Fetch failed:', error));
   }, []);
 
+  // Todoの追加
   const handleAddTodo = async (e: React.FormEvent) => {
+    // ページのリロードを防ぐ
     e.preventDefault();
 
+    // 空文字を棄却
     if (!title.trim()) return;
 
+    // エラーハンドリング
     try {
+      // サーバーにPOSTリクエストを送って、新しいtodoを追加
+      // fetch によって、指定したURLにHTTPリクエストを送る
       const response = await fetch(`${API_BASE}/add`, {
+        // HTTPリクエストはPOST
         method: 'POST',
+        // json形式で送ることを明示
         headers: { 'Content-Type': 'application/json' },
+        // titleをjsonに変更
         body: JSON.stringify({ title }),
       });
 
+      // レスポンスが正常であれば、todosを更新
       if (response.ok) {
+        // message(成功メッセージ) と newTodo(入力された新しいtodo) を分割代入により受け取る
         const { message, newTodo } = await response.json();
         console.log(message);
+        // 新しい todo を配列の末尾に追加
         setTodos((prevTodos) => [...prevTodos, newTodo]);
+        // 入力フィールドの初期化
         setTitle("");
       } else {
+        // レスポンスが正常でない場合の処理
+        console.error('Failed to add todo');
+      }
+    } catch (error) {
+      console.error('Error:', error);
+    }
+  };
+
+  // Todoの削除
+  const handleDelete = async (id: number) => {
+    try {
+      // 
+      // 指定したURLにHTTPリクエストを送るために、fetchを使う
+      const response = await fetch(`${API_BASE}/todos/${id}`, {
+        // HTTPリクエストはDELETE
+        method: "DELETE",
+        // json形式で送ることを明示
+        headers: { 'Content-Type': 'application/json' },
+      });
+
+      if (response.ok) {
+        // 
+        const { message, id } = await response.json();
+        console.log(message);
+        // 現在のTodosオブジェクト配列をコピー
+        const newTodos: Todo[] = [...todos];
+        // 削除ボタンが押されたidのオブジェクトを除外
+        const filterTodos = newTodos.filter((todo: Todo) => todo.id != id);
+        // Todosオブジェクト配列を更新
+        setTodos(filterTodos);
+      } else {
+        // レスポンスが正常でない場合の処理
         console.error('Failed to add todo');
       }
     } catch (error) {
@@ -65,6 +110,9 @@ function App() {
         {todos.map((todo) => (
           <li key={todo.id}>
             {todo.title} {todo.completed ? '✔️' : '❌'}
+            <button onClick={() => handleDelete(todo.id)}>
+              削除
+            </button>
           </li>
         ))}
       </ul>
