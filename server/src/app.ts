65a6ccd7af566474a8f31721ab03000a49d89274
yryo410 <ryoya20020410@gmail.com>
2025-04-09 18:08:03 +0900

    complete addtodo

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,10 +1,11 @@
 import express from 'express';
+import cors from 'cors';
 import todoRoutes from './routes/todoRoutes';
 
 const app = express();
 
 app.use(express.json());
-
+app.use(cors());
 // ルーティング設定（例: /api/todos）
 app.use('/api', todoRoutes);
 
