0eba12dd7188bbeb79d51f224d3ac625e73466cf
横山湧 <yoko0@MacBook-Air.local>
2025-04-09 11:38:31 +0900

    [add]Todoを追加する機能の追加

--- server/src/controllers/todoController.ts
+++ server/src/controllers/todoController.ts
@@ -1,8 +1,17 @@
 import { Request, Response } from 'express';
+import { todo } from 'node:test';
+
+const todos = [
+  { id: 1, title: '掃除する', completed: false },
+  { id: 2, title: '勉強する', completed: true },
+]
 
 export const getTodos = (req: Request, res: Response) => {
-  res.json([
-    { id: 1, title: '掃除する', completed: false },
-    { id: 2, title: '勉強する', completed: true },
-  ]);
+  res.json(todos);
 };
+
+export const addTodos = (req: Request, res: Response) => {
+  const testjson = { id: 3, title: '追加', completed: false };
+  todos.push(testjson);
+  res.status(201).json(testjson);
+}
